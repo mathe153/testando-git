b6716ad4a5514f1f6b9d85f81e8cb071b93e8ccd
matheus ribeiro <matheusribeiro705@gmail.com>
2024-02-21 23:26:03 -0300

    commit: "atualizando"

---
+++ objetos.js
@@ -0,0 +1,5 @@
+class formaDeBolo(
+    constructor(){
+        
+    }
+)
---
+++ teste.js
@@ -0,0 +1,13 @@
+function combinandoNomesPokemons(palavra){
+  let palavraPokemon = palavra + 'saur';
+    return palavraPokemon
+}
+
+// Entrada da palavra usando o gets():
+let nomeEntrada = gets(bulba);
+
+// Chamando a função "combinandoNomesPokemons" com o nome pokemon informado e armazenando o resultado na variável "palavraGerada":
+let palavraGerada = nomeEntrada(palavraPokemon);
+
+// Exibindo a palavra gerada:
+print(palavraGerada);
